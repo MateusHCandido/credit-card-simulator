f71056c2d4a4560514f34e1092d403c9ba2de6e9
Mateuzo <mateushcandido5@gmail.com>
2024-06-04 17:23:24 -0300

    creation of cloud gateway service

---
+++ cloud-gateway-service/src/test/java/com/github/MateusHCandido/cloud_gateway_service/CloudGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.MateusHCandido.cloud_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
